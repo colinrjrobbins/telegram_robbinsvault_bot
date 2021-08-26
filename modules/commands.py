67aafc4bfb2fb311026980f8076a414415e6ec7a
Colin Robbins <colinrjrobbins@gmail.com>
2021-08-26 12:22:32 -0400

    Added full login in and logout features with reusability.

--- modules/commands.py
+++ modules/commands.py
@@ -1,5 +1,6 @@
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
+from telegram.ext import ConversationHandler
 import socket
 
 ## TODO ##
@@ -18,33 +19,68 @@ class Commands:
         self.__dispatcher.add_handler(self.__start_handler)
         self.__ip_address_handler = CommandHandler('ipadd', self.getip)
         self.__dispatcher.add_handler(self.__ip_address_handler)
-        self.__echo_handler = MessageHandler(Filters.text & (~Filters.command), self.echo)
-        self.__dispatcher.add_handler(self.__echo_handler)
+        self.__logout_handler = CommandHandler('logout', self.logout)
+        self.__dispatcher.add_handler(self.__logout_handler)
         self.__username = username
         self.__password = password
+        self.__username_holder = ""
+        self.__password_holder = ""
         self.logged_in = False
 
-    def login_check(self, update, context):
+        self.USERNAME, self.PASSWORD = range(2)
+
+        self.__login_handler = ConversationHandler(
+            entry_points=[CommandHandler('login', self.login)],
+            states={
+                self.USERNAME: [MessageHandler(Filters.text & ~Filters.command, self.login_check)],
+                self.PASSWORD: [MessageHandler(Filters.text & ~Filters.command, self.password_check)]
+            },
+            fallbacks=[CommandHandler('cancel', self.logout)],
+            allow_reentry=True
+        )
+        self.__dispatcher.add_handler(self.__login_handler)
+
+    def logout(self, update, context):
         if self.logged_in == False:
-            if update.message.text == self.__username:
-                context.bot.send_message(chat_id=update.effective_chat.id,
-                                         text="Password:")
-                self.__password_handler = MessageHandler(Filters.text & (~Filters.command), self.password_check)
-                self.__dispatcher.add_handler(self.__password_handler)
+            pass
+        else:
+            self.logged_in = False
+            update.message.reply_text("Goodbye. Logging out now.")
+
+    def login(self, update, context):
+        if self.logged_in == False:
+            update.message.reply_text("Please enter your username:")
+            return self.USERNAME
+        else:
+            pass
+
+    def login_check(self, update, context):
+        self.__username_holder = update.message.text
+        if self.logged_in == False:
+            if self.__username_holder == self.__username:
+                update.message.reply_text("Please enter your password:")
+                return self.PASSWORD
             else:
                 context.bot.send_message(chat_id=update.effective_chat.id,
                                          text="Incorrect Username, please try again.")
+                return self.login(update,context)
         else:
             pass
 
     def password_check(self, update, context):
+        self.__password_holder = update.message.text
+        context.bot.deleteMessage(update.message.chat_id, update.message.message_id)
         if self.logged_in == False:
-            if update.message.text == self.__password:
+            if self.__password_holder == self.__password:
                 context.bot.send_message(chat_id=update.effective_chat.id,
                                          text="Logged in to RobbinsVault." + \
                                               "\n\nWelcome " + self.__username)
                 self.logged_in = True
                 self.start(update,context)
+            else:
+                context.bot.send_message(chat_id=update.effective_chat.id,
+                                         text="Incorrect password, please try again.")
+                return self.login(update,context)
         else:
             pass
 
@@ -52,24 +88,14 @@ class Commands:
         if self.logged_in == False:
             context.bot.send_message(chat_id=update.effective_chat.id,
                                      text="Welcome to RobbinsVault, " +\
-                                     "You will now be prompted to log in.\n\n" +\
-                                     "Please enter your Username")
-            self.__username_handler = MessageHandler(Filters.text & (~Filters.command), self.login_check)
-            self.__dispatcher.add_handler(self.__username_handler)
+                                     "To login type /login.")
         else:
             context.bot.send_message(chat_id=update.effective_chat.id, 
                                      text="Welcome to RobbinsVault, "+\
                                           "please enter a command.\n\n" +\
-                                          "/start      Restart the Bot / Show Help Options\n" +\
-                                          "/ipadd     Get the system IP Address\n"+\
-                                          "\nAnything else entered will be echoed.")
-
-    def echo(self, update,context):
-        if self.logged_in == False:
-            pass
-        else:
-            context.bot.send_message(chat_id=update.effective_chat.id, 
-                                     text=update.message.text)
+                                          "/start  - Restart the Bot / Show Help Options\n" +\
+                                          "/ipadd  - Get the system IP Address\n"+\
+                                          "/logout - Logout of the system.")
 
     def getip(self, update,context):
         if self.logged_in == False:
@@ -85,4 +111,3 @@ class Commands:
                 st.close()
             context.bot.send_message(chat_id=update.effective_chat.id, 
                                     text=IP)
-
